8e2296e5f8f290c4e19549a48c9d3b976a898149
Abhimanyuten <66128127+Abhimanyuten@users.noreply.github.com>
2020-10-12 18:59:02 +0530

    Update Answer.java
    Corrected the answer

--- Factorial/Answer.java
+++ Factorial/Answer.java
@@ -6,21 +6,24 @@ public class Factorial {
 		
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		int a = 0;
-		int b=1;
-		int f= 0;
-		System.out.println(a + " ");
-		System.out.println(b + " ");
-		
-		for(int i=0; i<n-2; i++) {
-			
-			System.out.println(f= a + b);
-			a=b;
-			b=f;
+		int factorial=1;
+		if (n==0)
+		{
+			factorial=factorial*1;
 		}
-		
+		else {
+			for(int i=1;i<=n;i++)
+			{
+				factorial=factorial*i;
+			}
+			
+		}
+		System.out.println(factorial);
 		
 		
 	}
 
+	
+
 }
+
