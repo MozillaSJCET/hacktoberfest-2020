32a846f647553c1de1e852729e413f816e319d11
Varghesemartin <71536249+Varghesemartin@users.noreply.github.com>
2020-10-13 13:56:57 +0530

    Add files via upload

---
+++ Factorial/Factorial.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+{
+int f=1,i,n;
+printf("enter the number");
+scanf("%d",&n);
+if(n==0)
+    printf("The factorial is 1");
+for(i=1;i<=n;i++)
+{
+
+ f=f*i;
+}
+printf("The factorial is %d",f);
+}
